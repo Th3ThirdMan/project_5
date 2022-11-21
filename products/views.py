620d95138beac26239c9a986014bc472770b6bc4
Th3ThirdMan <davidgkennedymail@gmail.com>
2022-11-21 15:31:10 +0000

    Add Product Details functionality and adjust Media Queries for Products

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 """ View to render all products page """
@@ -13,3 +13,16 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_details(request, product_id):
+
+    """ View to show individual product items """
+    
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_details.html', context)
