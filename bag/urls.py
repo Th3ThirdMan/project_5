c8a5fcc74b28fa6530e6495491d6ffa479a54bd5
Th3ThirdMan <davidgkennedymail@gmail.com>
2022-11-23 19:21:25 +0000

    Add JS functionality for adding items to bag

--- bag/urls.py
+++ bag/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.bag_view, name='bag_view'),
     path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
+    path('adjust/<item_id>/', views.adjust_bag, name='adjust_bag'),
 ]
